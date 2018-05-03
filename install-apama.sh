472965994e19c523de3ddfe14ba681390ca24e11
Richard Peach (RPEA) <Richard.Peach@softwareag.com>
2018-05-03 11:03:44 +0100

    Adding expect to run apama install

--- install-apama.sh
+++ install-apama.sh
@@ -1,3 +1,3 @@
 wget https://downloads.apamacommunity.com/apama/10.2.0.1/apama_10.2.0.1_amd64_linux.zip  -O /tmp/apama.zip
 unzip /tmp/apama.zip -d /tmp/
-printf 'N\n' | /tmp/apama_10.2.0.1_amd64_linux/install -installDir $(pwd)/softwareag
+expect -f ./install-apama-prompts.sh
